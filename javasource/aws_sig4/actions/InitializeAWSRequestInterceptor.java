fc6cb4811b5d8fa6836b694fa87d3be3cf69364b
Simon Black <simonblack1@hotmail.com>
2022-05-31 15:26:25 +0000

    Working on some base styling.

--- javasource/aws_sig4/actions/InitializeAWSRequestInterceptor.java
+++ javasource/aws_sig4/actions/InitializeAWSRequestInterceptor.java
@@ -44,6 +44,7 @@ public class InitializeAWSRequestInterceptor extends CustomJavaAction<java.lang.
 
 	/**
 	 * Returns a string representation of this action
+	 * @return a string representation of this action
 	 */
 	@java.lang.Override
 	public java.lang.String toString()
